e636d70ceb453cce6564ba02a194129113c23a26
Max O'Donovan <mcod1999yt@gmail.com>
2020-04-17 19:23:09 +0100

    Platform moves when player steps on it

---
+++ Group Project/Assets/Platforms/platform_trigger_script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platform_trigger_script : MonoBehaviour
+{
+
+    public movingPlatformScript platform;
+    private void OnTriggerEnter(Collider other)
+    {
+        platform.NextPlatform();
+    }
+}

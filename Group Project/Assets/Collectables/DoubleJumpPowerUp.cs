e04f7b5b6acb1d622d5621fcb97f69873d55a09d
Unknown <Claudia2.Brown@live.uwe.ac.uk>
2020-04-19 18:23:53 +0100

    collectables == depression

---
+++ Group Project/Assets/Collectables/DoubleJumpPowerUp.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DoubleJumpPowerUp : MonoBehaviour
+{
+    public GameObject DoubleJump;
+    public float multiplier = 3f;
+    public float duration = 10f;
+    
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(PickUp(other));
+        }
+
+    }
+
+   
+
+    IEnumerator PickUp(Collider player)
+    {
+        Debug.Log("Double jump picked up!");
+        Destroy(gameObject);
+       
+        Instantiate(DoubleJump, transform.position, transform.rotation);
+
+        NewMovement3D GetDoubleJump = player.GetComponent<NewMovement3D>();
+        
+
+        
+
+
+
+
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        yield return new WaitForSeconds(duration);
+
+        
+
+
+     
+        
+
+    }
+
+
+}
+
---
+++ Group Project/Assets/Collectables/SpeedPowerUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPowerUp : MonoBehaviour
+{
+    public GameObject SpeedBoostEffect;
+    //public float multiplier = 1.4f;
+    public float duration = 5f;
+    public NewMovement3D player;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(PickUp(other));
+            player.speedBoost = true;
+        }
+
+    }
+
+        IEnumerator PickUp(Collider player)
+        {
+            Debug.Log("Speed Boost picked up!");
+            //NewMovement3D speed = player.GetComponent<NewMovement3D>();
+            SpeedBoostEffect = Instantiate(SpeedBoostEffect, transform.position, transform.rotation);
+       
+
+            //speed.playerSpeed *= multiplier;
+            Destroy(gameObject);
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+            yield return new WaitForSeconds(duration);
+            //speed.playerSpeed /= multiplier;
+
+
+        }
+    
+   
+}

717be77e188a43a0a5585a0512454e35d4bd15b3
Max O'Donovan <mcod1999yt@gmail.com>
2020-04-19 17:36:16 +0100

    Started merge

---
+++ Group Project/Assets/GUI/player_coins.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_coins : MonoBehaviour
+{
+
+    public int coins;
+    public Text coin_count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        coin_count.text = "x " + coins;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coin_count.text = "x " + coins;
+    }
+
+    public void addCoin()
+    {
+        coins++;
+    }
+}
---
+++ Group Project/Assets/GUI/player_health.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_health : MonoBehaviour
+{
+
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHearts;
+    public Sprite emptyHearts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        for(int i =0; i < hearts.Length; i++)
+        {
+            if(i < health)
+            {
+                hearts[i].sprite = fullHearts;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHearts;
+            }
+
+            if( i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
+    public void addHealth(int health_to_add)
+    {
+        health += health_to_add;
+    }
+
+    public void removeHealth()
+    {
+        health--;
+    }
+}

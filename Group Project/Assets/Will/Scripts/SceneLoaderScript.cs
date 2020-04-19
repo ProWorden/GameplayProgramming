68e872a4198ab66793b845e2fbaf25b97c9e7009
Unknown <william4.james@live.uwe.ac.uk>
2020-04-19 16:12:21 +0100

    Hopefully this works

---
+++ Group Project/Assets/Will/Scripts/SceneLoaderScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderScript : MonoBehaviour
+{
+    [SerializeField]
+    Animator transition;
+    [SerializeField]
+    float transitionTime = 1f;
+    IEnumerator ChangeScene(string scene)
+    {
+        transition.SetTrigger("StartScene");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(scene);
+    }
+
+    public void LoadScene(string scene_name)
+    {
+        StartCoroutine(ChangeScene(scene_name)); 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

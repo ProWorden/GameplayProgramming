bd3be440c4bc6660893bf7e0ab12ee0f9381001b
Unknown <Christopher2.Worden@live.uwe.ac.uk>
2020-04-21 20:06:41 +0100

    added key to game

---
+++ Group Project/Assets/Worden/Key.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+     public globalControl saveData;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            print("hey");
+            Debug.Log("HIT!");
+            saveData.hasKey = true;
+            Destroy(this.gameObject);
+        }
+    }
+
+}

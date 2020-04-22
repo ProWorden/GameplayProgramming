c7f761cbd6ebd8c4192e4d995b4110aee53d60e7
Unknown <Claudia2.Brown@live.uwe.ac.uk>
2020-04-22 15:01:03 +0100

    pls work

---
+++ Group Project/Assets/Collectables/DoubleJumpPowerUp.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DoubleJumpPowerUp : MonoBehaviour
+{
+    public GameObject DoubleJump;
+    public float multiplier = 3f;
+    public float duration = 10f;
+    public NewMovement3D player;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(PickUp(other));
+            player.doubleJump = true;
+        }
+
+    }
+
+   
+
+    IEnumerator PickUp(Collider player)
+    {
+        Debug.Log("Double jump picked up!");
+        
+       
+        DoubleJump = Instantiate(DoubleJump, transform.position, transform.rotation);
+
+        NewMovement3D GetDoubleJump = player.GetComponent<NewMovement3D>();
+
+        Destroy(gameObject);
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        yield return new WaitForSeconds(duration);
+
+        
+
+
+     
+        
+
+    }
+
+
+}
+

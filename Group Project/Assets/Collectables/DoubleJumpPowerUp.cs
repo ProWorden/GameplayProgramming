7fcc00cf747fd77ffcb149cf9f038e7f8cd2f3e4
Unknown <Claudia2.Brown@live.uwe.ac.uk>
2020-04-22 14:40:15 +0100

    Double Jumpin to my grave

--- Group Project/Assets/Collectables/DoubleJumpPowerUp.cs
+++ Group Project/Assets/Collectables/DoubleJumpPowerUp.cs
@@ -8,13 +8,15 @@ public class DoubleJumpPowerUp : MonoBehaviour
     public GameObject DoubleJump;
     public float multiplier = 3f;
     public float duration = 10f;
-    
-    
+    public NewMovement3D player;
+
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             StartCoroutine(PickUp(other));
+            player.doubleJump = true;
         }
 
     }
@@ -24,18 +26,13 @@ public class DoubleJumpPowerUp : MonoBehaviour
     IEnumerator PickUp(Collider player)
     {
         Debug.Log("Double jump picked up!");
-        Destroy(gameObject);
+        
        
-        Instantiate(DoubleJump, transform.position, transform.rotation);
+        DoubleJump = Instantiate(DoubleJump, transform.position, transform.rotation);
 
         NewMovement3D GetDoubleJump = player.GetComponent<NewMovement3D>();
-        
 
-        
-
-
-
-
+        Destroy(gameObject);
         GetComponent<MeshRenderer>().enabled = false;
         GetComponent<Collider>().enabled = false;
         yield return new WaitForSeconds(duration);

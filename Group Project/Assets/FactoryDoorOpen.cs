8af03ac9dc81b0b94e603c7fb0e4d8d0799b15d8
Unknown <Christopher2.Worden@live.uwe.ac.uk>
2020-04-22 14:55:11 +0100

    Creating door, trying to get collision to play animation

---
+++ Group Project/Assets/FactoryDoorOpen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactoryDoorOpen : MonoBehaviour
+{
+    public Animation anim;
+    public globalControl GC;
+    float timer = 0;
+    bool openingDoor = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && GC.hasKey)
+        {
+            anim.Play();
+
+            openingDoor = true;
+            
+        }
+    }
+
+    private void Update()
+    {
+        if(openingDoor&& timer <=1)
+        {
+            timer += Time.deltaTime;
+            
+        }
+        else
+        {
+            anim.Stop();
+        }
+    }
+}

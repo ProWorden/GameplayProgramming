95338af5a7ca898520c64122f9c0e4802e1c0a9f
Unknown <Christopher2.Worden@live.uwe.ac.uk>
2020-04-22 17:06:59 +0100

    added key spawn above head when opening door

---
+++ Group Project/Assets/SpawnKey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnKey : MonoBehaviour
+{
+    public MeshRenderer render;
+    public FactoryDoorOpen opening;
+    float timer = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(opening.openingDoor && timer <=1)
+        {
+            render.enabled = true;
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            render.enabled = false;
+        }
+    }
+}

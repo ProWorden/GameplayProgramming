8b5e76dfbee7198ba723d1c5ceac65272f0b0836
Max O'Donovan <mcod1999yt@gmail.com>
2020-04-22 17:17:45 +0100

    Win screen

---
+++ Group Project/Assets/win_game.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class win_game : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject sceneLoader;
+    [SerializeField]
+    string scene_to_load;
+    [SerializeField]
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+
+            sceneLoader.GetComponent<SceneLoaderScript>().LoadScene(scene_to_load);
+
+        }
+    }
+}
